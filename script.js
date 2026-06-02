4684c8e614fd6c2dfa8e5c87ba18fe9284cdfca1
razyshafapratama-droid <razyshafapratama@gmail.com>
2026-06-02 09:19:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Ambil semua section dan link navigasi
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.nav-links a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
